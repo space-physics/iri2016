8887b85e9ca81f8c6234cebf72c0c8796c52a276
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-16 00:19:15 -0400

    init

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from numpy.testing import run_module_suite,assert_allclose
+from pyiri2016 import IRI2016
+
+def test_main1():
+
+    Obj = IRI2016()
+    IRIData, IRIDATAAdd = Obj.IRI()
+    assert_allclose((IRIData['ne'], IRIDATAAdd['NmF2'], IRIDATAAdd['hmF2']),
+                    (267285184512.0,2580958937088.0,438.78643798828125))
+
+if __name__ == '__main__':
+    run_module_suite()
