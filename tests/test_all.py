12a2385160959126cb9a11a30718bf7e42c2bd6d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-27 03:01:31 -0500

    unittest

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-from numpy.testing import run_module_suite,assert_allclose
+import numpy as np
+import unittest
 from pyiri2016 import IRI2016
 
-def test_main1():
+class BasicTests(unittest.TestCase):
+    def test_main1(self):
 
-    Obj = IRI2016()
-    IRIData, IRIDATAAdd = Obj.IRI(time='1980-03-21T12')
-    assert_allclose((IRIData['ne'], IRIDATAAdd['NmF2'], IRIDATAAdd['hmF2']),
-                    (267285184512.0, 2580958937088.0, 438.78643798828125))
+        Obj = IRI2016()
+        IRIData, IRIDATAAdd = Obj.IRI(time='1980-03-21T12')
+        np.testing.assert_allclose((IRIData['ne'], IRIDATAAdd['NmF2'], IRIDATAAdd['hmF2']),
+                        (267285184512.0, 2580958937088.0, 438.78643798828125))
 
 if __name__ == '__main__':
-    run_module_suite()
+    unittest.main()
