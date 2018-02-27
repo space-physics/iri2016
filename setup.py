521d6ad92960ee261265b5d8e134280d72f8326c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-27 00:37:46 -0500

    prereq

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-install_requires = ['numpy','pathlib2',
+install_requires = ['python-dateutil','numpy','pathlib2',
                     'timeutil']
 tests_require = ['nose','coveralls']
 name = 'pyiri2016'
