45f829dbcd2508ccb3253662636e6c5967e048b8
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-27 00:47:47 -0500

    cleanup API

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,7 +5,7 @@ from pyiri2016 import IRI2016
 def test_main1():
 
     Obj = IRI2016()
-    IRIData, IRIDATAAdd = Obj.IRI(time='1980-03-21')
+    IRIData, IRIDATAAdd = Obj.IRI(time='1980-03-21T12')
     assert_allclose((IRIData['ne'], IRIDATAAdd['NmF2'], IRIDATAAdd['hmF2']),
                     (267285184512.0, 2580958937088.0, 438.78643798828125))
 
