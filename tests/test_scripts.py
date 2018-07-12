6a550bbca3aec577d138d069758ea12517c3935a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-12 17:00:20 -0400

    default quiet tests

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -7,15 +7,15 @@ root = Path(__file__).parents[1]
 
 
 def test_latitude():
-    subprocess.check_call(['IRIlatitudeProfile', '-148'])
+    subprocess.check_call(['IRIlatitudeProfile', '-148','-q'])
 
 
 def test_time():
-    subprocess.check_call(['IRItimeProfile', '65', '-148'])
+    subprocess.check_call(['IRItimeProfile', '65', '-148','-q'])
 
 
 def test_alt():
-    subprocess.check_call(['IRIaltitudeProfile', '65', '-148'])
+    subprocess.check_call(['IRIaltitudeProfile', '65', '-148','-q'])
 
 
 if __name__ == '__main__':
