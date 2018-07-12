22f9a8d4da3d4efcf76bc35e2ec718848319089d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-12 16:09:16 -0400

    init

---
+++ tests/test_scripts.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import pytest
+import subprocess
+from pathlib import Path
+
+root = Path(__file__).parents[1]
+
+
+def test_latitude():
+    subprocess.check_call(['IRIlatitudeProfile', '-148'])
+
+
+def test_time():
+    subprocess.check_call(['IRItimeProfile', '65', '-148'])
+
+
+def test_alt():
+    subprocess.check_call(['IRIaltitudeProfile', '65', '-148'])
+
+
+if __name__ == '__main__':
+    pytest.main()
