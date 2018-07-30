639959fac79a6466cde4989636c8cf123f67acfe
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-30 16:58:53 -0400

    mypy

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -15,17 +15,16 @@ def test_point():
                                (267285184512.0, 2580958937088.0, 438.78643798828125))
 
 
-
 def test_altitude_profile():
     time = '2015-12-13T10'
-    alt_km = np.arange(100,1000,10)
+    alt_km = np.arange(100, 1000, 10)
     glat = 65.1
     glon = -147.5
 
     iri = iri2016.IRI(time, alt_km, glat, glon).squeeze()
 
     np.testing.assert_allclose(iri['ne'][10], 4.931192e+09)
-    np.testing.assert_allclose((iri.NmF2, iri.hmF2),(82.14109e9, 317.35287))
+    np.testing.assert_allclose((iri.NmF2, iri.hmF2), (82.14109e9, 317.35287))
 
 
 if __name__ == '__main__':
