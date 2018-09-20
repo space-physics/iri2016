ab23f1a96041b8aa2aaaae8358cdd16f8a9f4a35
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-09-20 10:22:34 -0400

    update Matlab API

--- README.md
+++ README.md
@@ -65,8 +65,7 @@ Here's what's you'll need:
    python3
    ```
    If you need to install Python, consider [Miniconda](https://conda.io/miniconda.html) as it's a small install (normally, use the 64-bit version).
-2. Matlab &ge; R2017b, as this is when Python 3.6 was enabled. 
-   If you're stuck with older Matlab &lt; R2017b, you can try to [force-enable Python 3.6](https://www.scivision.co/matlab-python-user-module-import/)
+2. Matlab &ge; R2014b
 3. The function [iri2016.m](iri2016.m) gives some examples of what you can do (run, plot) IRI2016 from Matlab calling Python (and ultimately the original Fortran code).
    The functions in that file `xarrayind2vector()` and `xarray2mat()` translate Python's advanced Xarray N-D data structures to Matlab arrays.
 
--- iri2016.m
+++ iri2016.m
@@ -2,6 +2,8 @@ function iri2016()
 % quick demo calling IRI2016 model from Matlab.
 % https://www.scivision.co/matlab-python-user-module-import/
 
+assert(~verLessThan('matlab', '8.4'), 'Matlab >= R2014b required')
+
 % geographic WGS84 lat,lon,alt
 glat = 65.1;
 glon = -147.5;
@@ -9,27 +11,33 @@ alt_km = 100:10:1000;
 t = '2015-12-13T10';
 
 
-dt = py.iri2016.IRI(t, alt_km, glat, glon);
+iono = py.iri2016.IRI(t, alt_km, glat, glon);
 
-Ne = xarray2mat(dt{'ne'});
-NmF2 = xarray2mat(dt{'NmF2'});
-hmF2 = xarray2mat(dt{'hmF2'});
-
-plotiono(alt_km, Ne, t, glat, glon)
+plotiono(iono)
 end
 
-function plotiono(alt_km, Ne, t,glat,glon)
-  figure(1), clf(1)
-  ax = axes('nextplot','add');
+function plotiono(iono)
 
-  semilogx(ax, Ne, alt_km)
-  
-  set(ax,'xscale','log')
-  title({[t,' deg.  (',num2str(glat),',', num2str(glon),')']})
-  xlabel('Density [m^-3]')
-  ylabel('altitude [km]')
+Ne = xarray2mat(iono{'ne'});
+NmF2 = xarray2mat(iono{'NmF2'});
+hmF2 = xarray2mat(iono{'hmF2'});
 
-  grid('on')
+alt_km = xarrayind2vector(iono, 'alt_km');
+times = char(iono.attrs{'time'}.isoformat());
+glat = iono.attrs{'glat'};
+glon = iono.attrs{'glon'};
+
+figure(1), clf(1)
+ax = axes('nextplot','add');
+
+semilogx(ax, Ne, alt_km)
+
+set(ax,'xscale','log')
+title({[times,' deg.  (',num2str(glat),', ', num2str(glon),')']})
+xlabel('Density [m^-3]')
+ylabel('altitude [km]')
+
+grid('on')
   
 end
 
@@ -46,12 +54,14 @@ end
 
 function I = xarrayind2vector(V,key)
 
-C = cell(V{1}.indexes{key}.values.tolist);  % might be numeric or cell array of strings
+C = cell(V.indexes{key}.values.tolist);  % might be numeric or cell array of strings
 
-if iscellstr(C) || any(class(C{1})=='py.str')
-    I=cellfun(@char,C, 'uniformoutput',false);
+if iscellstr(C) || isa(C{1}, 'py.str')
+    I = cellfun(@char, C, 'uniformoutput', false);
+elseif isa(C{1}, 'py.datetime.datetime')
+    I = char(C{1}.isoformat());
 else
-    I = cell2mat();
+    I = cell2mat(C);
 end % if
 
 end % function
