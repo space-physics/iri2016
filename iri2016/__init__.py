ff26f30cf791fa5dd169fa57638d795b60a3a9e5
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-11-26 02:38:06 -0500

    windows shell workarounds

--- iri2016/__init__.py
+++ iri2016/__init__.py
@@ -8,8 +8,10 @@ import numpy as np
 
 R = Path(__file__).resolve().parents[1] / 'bin'
 EXE = './iri2016_driver'
+SHELL = False
 if os.name == 'nt':
     EXE = EXE[2:] + '.exe'
+    SHELL = True
 
 SIMOUT = ['ne', 'Tn', 'Ti', 'Te', 'nO+', 'nH+', 'nHe+', 'nO2+', 'nNO+',
           'nCI', 'nN+']
@@ -23,12 +25,16 @@ def IRI(time, altkmrange, glat: float, glon: float) -> xarray.Dataset:
     assert len(altkmrange) == 3, 'altitude (km) min, max, step'
     assert isinstance(glat, float) and isinstance(glon, float), 'for now, glat, glon is scalar'
 
-    ret = subprocess.check_output([str(EXE),
-                                   str(time.year), str(time.month), str(time.day),
-                                   str(time.hour), str(time.minute), str(time.second),
-                                   str(glat), str(glon),
-                                   str(altkmrange[0]), str(altkmrange[1]), str(altkmrange[2])],
-                                  universal_newlines=True, cwd=R)
+    # NOTE: Windows needs shell=True and str(pathlib.Path)
+    cmd = [str(EXE),
+           str(time.year), str(time.month), str(time.day),
+           str(time.hour), str(time.minute), str(time.second),
+           str(glat), str(glon),
+           str(altkmrange[0]), str(altkmrange[1]), str(altkmrange[2])]
+
+    ret = subprocess.check_output(cmd,
+                                  universal_newlines=True, cwd=str(R),
+                                  shell=SHELL)
 
     arr = np.loadtxt(io.StringIO(ret))
 
