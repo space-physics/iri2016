586f1665b8a4851e141a961ef35e35ea80d64f5a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-14 13:28:30 -0500

    make build dirs robust [skip appveyor]

--- matlab/setup_iri2016.m
+++ matlab/setup_iri2016.m
@@ -1,16 +1,25 @@
 %% setup IRI2016 using Fortran compiler
 
+system('cmake --version')
+
+cwd = fileparts(mfilename('fullpath'));
+
+srcdir =   [cwd, filesep,'..',filesep,'src'];
+builddir = [cwd, filesep,'..',filesep,'build'];
+
+tail = [' -S ', srcdir, ' -B ', builddir];
+
 if ispc
-  ccmd = 'cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -S ../src -B ../build';
+  ccmd = ['cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" ', tail];
 else
-  ccmd = 'cmake -S ../src -B ../build';
+  ccmd = ['cmake ',tail];
 end
 
 [status, ret] = system(ccmd);
 if status~=0, error(ret), end
 disp(ret)
 
-[status, ret] = system('cmake --build ../build -j');
+[status, ret] = system(['cmake --build ',builddir,' -j']);
 if status~=0, error(ret), end
 disp(ret)
 
