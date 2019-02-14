ad87bb698cf4f52fc56f0ff0af888f8590cfc60a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-14 12:09:19 -0500

    correct matlab build paths

--- matlab/setup_iri2016.m
+++ matlab/setup_iri2016.m
@@ -1,16 +1,16 @@
 %% setup IRI2016 using Fortran compiler
 
 if ispc
-  ccmd = ['cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -S ..\src -B build'];
+  ccmd = 'cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -S ../src -B ../build';
 else
-  ccmd = ['cmake -S ../src -B build'];
+  ccmd = 'cmake -S ../src -B ../build';
 end
 
 [status, ret] = system(ccmd);
 if status~=0, error(ret), end
 disp(ret)
 
-[status, ret] = system('cmake --build build -j');
+[status, ret] = system('cmake --build ../build -j');
 if status~=0, error(ret), end
 disp(ret)
 
