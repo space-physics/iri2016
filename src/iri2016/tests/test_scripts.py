37f983540c471b6b63640c42f2b357641bf5b316
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-07-24 03:48:49 -0400

    ci skipif

--- src/iri2016/tests/test_scripts.py
+++ src/iri2016/tests/test_scripts.py
@@ -4,19 +4,19 @@ import subprocess
 import os
 
 
-@pytest.mark.skipif(os.environ.get("CI"), reason="CI no display")
+@pytest.mark.skipif(os.environ.get("CI") is not None, reason="CI no display")
 def test_latitude():
     pytest.importorskip("matplotlib")
     subprocess.check_call(["iri2016_latitude", "-148"])
 
 
-@pytest.mark.skipif(os.environ.get("CI"), reason="CI no display")
+@pytest.mark.skipif(os.environ.get("CI") is not None, reason="CI no display")
 def test_time():
     pytest.importorskip("matplotlib")
     subprocess.check_call(["iri2016_time", "2012-01-01", "2012-01-02", "1.0", "65", "-148"])
 
 
-@pytest.mark.skipif(os.environ.get("CI"), reason="CI no display")
+@pytest.mark.skipif(os.environ.get("CI") is not None, reason="CI no display")
 def test_alt():
     pytest.importorskip("matplotlib")
     subprocess.check_call(["iri2016_altitude", "2012-01-01", "65", "-148"])
