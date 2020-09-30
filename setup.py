31fc7381dffb374571f9148dc1c0d937fff78b73
Michael Hirsch <scivision@users.noreply.github.com>
2020-09-29 22:03:35 -0400

    remove deprecated data_files

--- setup.py
+++ setup.py
@@ -1,49 +1,12 @@
 #!/usr/bin/env python3
 import setuptools
-from pathlib import Path
 import site
 
 # PEP517 workaround
 site.ENABLE_USER_SITE = True
 
+setuptools.setup()
+
 """
-Because of bad bugs in IRI2016 itself, present even in plain Fortran usage, we can't safely use F2py, bad data can result.
-
-src = [  # 'iriwebg.for',
-    'irisub.for', 'irifun.for',
-    'iritec.for', 'iridreg.for', 'igrf.for', 'cira.for', 'iriflip.for']
-
-F = Path('src')
-src = [str(F/s) for s in src]
-
-ext = Extension(name='iri16', sources=src,
-                f2py_options=['only:', 'iri_sub', ':'],  # ['skip:', 'dfridr', ':'],
-                extra_f77_compile_args=['-w'])
+Because of bugs in IRI2016 itself, present even in plain Fortran usage, we can't safely use F2py, bad data can result.
 """
-
-R = Path("src/iri2016/data")
-if not R.is_dir():
-    raise FileNotFoundError(R)
-
-iridata = list(
-    map(
-        str,
-        (
-            list((R / "ccir").glob("*.asc"))
-            + list((R / "igrf").glob("*.dat"))
-            + list((R / "index").glob("*.dat"))
-            + list((R / "mcsat").glob("*.dat"))
-            + list((R / "ursi").glob("*.asc"))
-        ),
-    )
-)
-
-if not iridata:
-    raise FileNotFoundError(f"iri data not found in {R}")
-
-iridata = list(map(str, iridata))  # even for Numpy 1.14 due to numpy.distutils
-
-setuptools.setup(
-    # ext_modules=[ext],
-    data_files=iridata
-)
